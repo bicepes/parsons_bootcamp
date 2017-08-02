7c029e99ea82be15c48fd457f1ca9f7587f931b8
kevinfan23 <kevinfan23@ucla.edu>
2017-08-02 17:32:12 -0400

    finished day3 lecture code

--- code/day3/lecture_3/build/source/lecture_3.java
+++ code/day3/lecture_3/build/source/lecture_3.java
@@ -14,28 +14,68 @@ import java.io.IOException;
 
 public class lecture_3 extends PApplet {
 
-int xPosition;
-int yPosition;
+/***************************************/
+      /*** Input Interations ***/
+/***************************************/
+
+// int value = 0;
+//
+// void setup() {
+//   size(500,500);
+//   colorMode(HSB);
+// }
+//
+// void draw() {
+//   if (mousePressed) {
+//     fill(value, 255, 255);
+//     ellipse(mouseX, mouseY, 30, 30);
+//   }
+//   //ellipse(xPosition,yPosition,100,100);
+//   if (value < 255) {
+//     value++;
+//   }
+//   else {
+//     value = 0;
+//   }
+// }
+//
+// void keyPressed() {
+//   if (keyPressed) {
+//     println(key);
+//     fill(255, 60, 40);
+//     if (key == 'a') {
+//       fill(255, 60, 40);
+//       println("a pressed");
+//     }
+//     if (key == 'z') {
+//       fill(40, 200, 15);
+//       println("z pressed");
+//     }
+//   }
+//   else {
+//     fill(20, 60, 255);
+//   }
+// }
+
+/***************************************/
+        /*** Load Assets ***/
+/***************************************/
+PImage newschool;
+PFont lato;
 
 public void setup() {
   
-
-  xPosition = width/2;
-  yPosition = height/2;
+  newschool = loadImage("newschool.jpg");
+  lato = createFont("Lato-Regular.ttf", 24);
 }
 
 public void draw() {
-  background(255);
-
-  if (keyPressed) {
-    fill(255, 60, 40);
-  }
-  else {
-    fill(20, 60, 255);
-  }
-  ellipse(xPosition,yPosition,100,100);
+  image(newschool, 0, 0, width, height);
+  textFont(lato, (40));
+  fill(255, 30, 64);
+  text("Hello!", random(700), random(500));
 }
-  public void settings() {  size(500,500); }
+  public void settings() {  size(700, 500); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "lecture_3" };
     if (passedArgs != null) {
--- code/day3/lecture_3/lecture_3.pde
+++ code/day3/lecture_3/lecture_3.pde
@@ -1,21 +1,61 @@
-int xPosition;
-int yPosition;
+/***************************************/
+      /*** Input Interations ***/
+/***************************************/
+
+// int value = 0;
+//
+// void setup() {
+//   size(500,500);
+//   colorMode(HSB);
+// }
+//
+// void draw() {
+//   if (mousePressed) {
+//     fill(value, 255, 255);
+//     ellipse(mouseX, mouseY, 30, 30);
+//   }
+//   //ellipse(xPosition,yPosition,100,100);
+//   if (value < 255) {
+//     value++;
+//   }
+//   else {
+//     value = 0;
+//   }
+// }
+//
+// void keyPressed() {
+//   if (keyPressed) {
+//     println(key);
+//     fill(255, 60, 40);
+//     if (key == 'a') {
+//       fill(255, 60, 40);
+//       println("a pressed");
+//     }
+//     if (key == 'z') {
+//       fill(40, 200, 15);
+//       println("z pressed");
+//     }
+//   }
+//   else {
+//     fill(20, 60, 255);
+//   }
+// }
+
+/***************************************/
+        /*** Load Assets ***/
+/***************************************/
+PImage newschool;
+PFont lato;
 
 void setup() {
-  size(500,500);
-
-  xPosition = width/2;
-  yPosition = height/2;
+  size(700, 500);
+  newschool = loadImage("newschool.jpg");
+  lato = createFont("Lato-Regular.ttf", 24);
 }
 
 void draw() {
-  background(255);
-
-  if (keyPressed) {
-    fill(255, 60, 40);
-  }
-  else {
-    fill(20, 60, 255);
-  }
-  ellipse(xPosition,yPosition,100,100);
+  image(newschool, 0, 0, width, height);
+  textFont(lato, (40));
+  fill(255, 30, 64);
+  text("Hello!", random(700), random(500));
 }
