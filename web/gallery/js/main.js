4b11b372f483ada9961ce0271a8f3cf6bcc01ee0
kevinfan23 <kevinfan23@ucla.edu>
2017-08-10 10:58:32 -0400

    display grid images

--- web/gallery/js/main.js
+++ web/gallery/js/main.js
@@ -1,3 +1,15 @@
 window.addEventListener("DOMContentLoaded", function(e) {
   console.log("hello!");
+  loadImages();
 });
+
+const imageNum = 15;
+const col = document.getElementsByClassName("column");
+
+function loadImages() {
+  for (var i = 0; i < imageNum; i++) {
+    var newImg = document.createElement("img");
+    newImg.src = ("./image/" + i + ".jpg");
+    col[i%col.length].appendChild(newImg);
+  }
+}
