1eadc686b1d58162a47704f1edaf45d7cdedd4b0
kevinfan23 <kevinfan23@ucla.edu>
2017-08-11 10:34:25 -0400

    center image regardless of image dimensions

--- web/gallery/js/main.js
+++ web/gallery/js/main.js
@@ -1,24 +1,58 @@
-window.addEventListener("DOMContentLoaded", function(e) {
-  console.log("hello!");
+window.addEventListener('DOMContentLoaded', function(e) {
+  console.log('hello!');
   //loadImages();
   loadImages();
 });
 
 const imageNum = 15;
-const col = document.getElementsByClassName("column");
+const col = document.getElementsByClassName('column');
+var curImg = 0;
 
 function loadImages() {
   for (var i = 0; i < imageNum; i++) {
-    var newImg = document.createElement("img");
-    newImg.src = ("./image/" + i + ".jpg");
+    var newImg = document.createElement('img');
+    newImg.src = ('./image/' + i + '.jpg');
+
+    newImg.id = i;
+    newImg.style.cursor = 'zoom-in'
+    newImg.addEventListener('click', function(e) {
+      curImg = e.target.id;
+      console.log(e.target);
+      popUp(e.target);
+    });
     col[i%col.length].appendChild(newImg);
   }
 }
 
+function popUp(img) {
+  var photoctn = document.getElementById('photo-container');
+  var photo = document.getElementById('pic');
+  photo.src = './image/' + curImg + '.jpg'
+  if (img.height > img.width) {
+    photo.style.height = '100%';
+    photo.style.width = 'auto';
+  }
+  else {
+    photo.style.width = '100%';
+    photo.style.height = 'auto';
+  }
+  photoctn.style.display = 'block';
+
+  photo.addEventListener('click', function(e) {
+    close();
+  });
+}
+
+function close() {
+  console.log("close");
+  var photoctn = document.getElementById('photo-container');
+  photoctn.style.display = 'none';
+}
+
 // function loadImages1() {
 //   for (var i = 0; i < imageNum; i++) {
-//     var newImg = document.createElement("img");
-//     newImg.src = ("./image/" + i + ".jpg");
+//     var newImg = document.createElement('img');
+//     newImg.src = ('./image/' + i + '.jpg');
 //     var shCol = getMin()
 //     col[shCol].appendChild(newImg);
 //   }
@@ -31,7 +65,7 @@ function loadImages() {
 //   for (var i = 0; i < col.length; i++) {
 //     var colHeight = col[i].clientHeight;
 //     heightArr[i] = colHeight;
-//     console.log("column" + i + "is: " + heightArr[i]);
+//     console.log('column' + i + 'is: ' + heightArr[i]);
 //   }
 //
 //   for (var i = 0; i < col.length; i++) {
